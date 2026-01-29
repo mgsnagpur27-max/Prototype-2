8b4ff7ec5b7628202edb9c180178204a23cb6f08
mgsnagpur27-max <mgsnagpur27@gmail.com>
2026-01-29 09:06:25 +0530

    Upload files prototype

---
+++ src/components/preview/index.ts
@@ -0,0 +1,4 @@
+export { PreviewFrame, type PreviewStatus } from './preview-frame';
+export { PreviewControls, type DeviceView } from './preview-controls';
+export { ConsolePanel } from './console-panel';
+export { ErrorOverlay } from './error-overlay';
