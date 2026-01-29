ee8a31d8ea0f7ffe30c3b33ce3bf689a4a64a7a8
mgsnagpur27-max <mgsnagpur27@gmail.com>
2026-01-29 09:07:56 +0530

    Upload files prototype

---
+++ src/lib/webcontainer/index.ts
@@ -0,0 +1,3 @@
+export { webContainerManager, type ContainerStatus, type WebContainerEvents, type ProcessInfo } from './manager';
+export { useWebContainer } from './use-webcontainer';
+export { PROJECT_TEMPLATES, getTemplateById, DEFAULT_FILES, type ProjectTemplate } from './templates';
